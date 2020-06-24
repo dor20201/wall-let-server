86780d2ea1713f976b480bfbf3c8195834675644
dor20201 <dor20201@users.noreply.github.com>
2020-06-24 21:49:10 +0300

    add userModule to the imports of statistics module

--- src/components/Statistics/statistics.module.ts
+++ src/components/Statistics/statistics.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { RequestModule } from '../Requests/request.module';
 import { StatisticsController } from './statistics.controller';
 import { StatisticsService } from './statistics.service';
+import { UserModule } from '../Users/user.module';
 
 
 @Module({
-  imports: [RequestModule],
+  imports: [RequestModule,UserModule],
   controllers: [StatisticsController],
   providers: [StatisticsService],
 })
