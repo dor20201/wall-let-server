30fb6e0162166896b84132b37b35df2b0e135b87
amitsz600 <38006612+amitsz600@users.noreply.github.com>
2020-08-17 15:31:45 +0300

    Add the rest function for the transaction after the request was approved

--- src/components/Financial/financial.controller.ts
+++ src/components/Financial/financial.controller.ts
@@ -1,10 +1,22 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { FinancialService } from './financial.service';
 import { UserService } from '../Users/user.service';
 
 @Controller('financial')
 export class FinancialController {
   constructor(private financialService: FinancialService, private _userService: UserService) {
+  }
+  @Get("creditCard/:id")
+  async getCreditCard(@Param('id') userId: string) {
+    const user = await this._userService.getUserById(userId);
+
+    // Check if the details are valid
+    if (user == null || !user.walletMember) {
+      return "user doesnt exist";
+    }
+
+
+    return this.financialService.findCreditCardByWalletMemberId(userId);
   }
 
   @Post("creditCard")
